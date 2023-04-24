0045d6c7e11db785a2469916a27397309213fc1d
Mohammad Zolfaghari <zidom72@gmail.com>
2023-04-24 10:35:46 +0330

    using starship for prompt

--- fish/config.fish
+++ fish/config.fish
@@ -14,32 +14,7 @@ end
 source /usr/share/icons-in-terminal/icons.fish
 
 set __prompt_icons $mfizz_ghost $file_gitlab $linux_debian $weather_day_storm_showers $mfizz_splatter $file_electron $fa_firefox $fa_google_wallet $mfizz_docker $mfizz_redhat
-set __prompt_icon (random 1 10)
+set __prompt_icon (math (date +%d) % 10)
 
-function fish_prompt
-	set last_status $status
 
-	set_color A6E22E
-	printf $__prompt_icons[$__prompt_icon]'  '
-
-	if test ! $TMUX;
-		set_color $fish_color_host
-		printf $USER'@%s ' (prompt_hostname)
-	end;
-
-	set_color $fish_color_cwd
-	set -l spwd (string split -r -m2 / $PWD)
-	set -l usr (string split / $HOME)
-	if [ "$HOME" = "$PWD" ]
-		echo -n "~"
-	else if [ "$spwd[2]" = "$usr[-1]" ]
-		echo -n "~/$spwd[3]"
-	else
-		echo -n "$spwd[2]/$spwd[3]"
-	end
-
-	set_color normal
-	printf '%s\n→ ' (__informative_git_prompt)
-
-	set_color normal
-end
+starship init fish | source
