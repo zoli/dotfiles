a28037f67ee1f54a076d0f26544e082137cbfbd5
Mohammad Zolfaghari <zidom72@gmail.com>
2024-01-27 13:39:21 +0330

    modified fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,19 @@
 set fish_greeting ''
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 set -gx PATH $PATH $HOME/.dotfiles/bin
 set -gx GOPATH $HOME/.go
-set -gx EDITOR vim
+set -gx EDITOR nvim
+set -gx TMUX_PLUGIN_MANAGER_PATH ~/.config/tmux/plugins/
 set -g FZF_DEFAULT_COMMAND 'fd -t f'
 set -g FZF_CD_COMMAND 'fd -t d -t l'
 
-if status is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
-    end
-end
+alias vim=nvim
+
+# if status is-login
+#     if test -z "$DISPLAY" -a $XDG_VTNR = 1
+#         exec startx -- -keeptty
+#     end
+# end
 
 source /usr/share/icons-in-terminal/icons.fish
 
