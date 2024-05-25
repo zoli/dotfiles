5c8d117f0dc0ca5f98baa4d5a89406b4d8e1e223
Mohammad Zolfaghari <zidom72@gmail.com>
2024-05-26 00:42:38 +0330

    fix fish icons, eww bar width

--- fish/config.fish
+++ fish/config.fish
@@ -15,7 +15,15 @@ alias vim=nvim
 #     end
 # end
 
-source /usr/share/icons-in-terminal/icons.fish
+set mfizz_ghost 󰊠
+set file_gitlab \uf296
+set linux_debian \uebc5
+set weather_day_storm_showers \ue30e
+set file_electron 
+set fa_firefox 󰈹
+set mfizz_docker \uf21f
+set mfizz_redhat \uef5d
+set mfizz_splatter 󰌽
 
 set __prompt_icons $mfizz_ghost $file_gitlab $linux_debian $weather_day_storm_showers $mfizz_splatter $file_electron $fa_firefox $mfizz_docker $mfizz_redhat
 set __prompt_icon (math (date +%d) % 9 + 1)
